a17245169720061cb6085c87411f8ad20ecff72e
jeonghyeon2 <gkswl412@naver.com>
2023-01-31 15:14:14 +0900

    [2023-01-31] pubspec.yaml 외 설정파일 추가

---
+++ calendar_scheduler/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:drift/drift.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+const DEFAULT_COLORS = [
+  // 빨강
+  'F44336',
+  // 주황
+  'FF9800',
+  // 노랑
+  'FFEB3B',
+  // 초록
+  'FCAF50',
+  // 파랑
+  '2196F3',
+  // 남
+  '3F51B5',
+  // 보라
+  '9C27B0',
+];
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      home: HomeScreen(),
+    ),
+  );
+}
