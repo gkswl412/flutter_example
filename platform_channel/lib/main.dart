8ab6c1b816a27a22ff1b3e278db19936acde7a3a
jeonghyeon2 <gkswl412@naver.com>
2023-02-15 20:24:34 +0900

    commit all

---
+++ platform_channel/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:platform_channel/screen/my_home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyHomePage(),
+    ),
+  );
+}
+
