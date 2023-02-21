71491b50f3afb6a9449473db7f2283f43d6a2c94
jeonghyeon2 <gkswl412@naver.com>
2023-02-21 14:58:36 +0900

    [2023-02-21] web_view : openlayers 지도, cctv 띄우기

--- web_view/lib/screen/home_screen.dart
+++ web_view/lib/screen/home_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomeScreen extends StatelessWidget {
   WebViewController? controller;
   HomeScreen({Key? key}) : super(key: key);
-  String homeUrl = 'https://blog.codefactory.ai';
+  String homeUrl = 'https://sc.hismart.co.kr/mobile/targetMap.do?targetId=9276&targetType=worker&targetName=%EA%B9%80%EB%AA%85%EC%A4%91&_csrf=e2be7636-bd28-4232-a429-d0ef8f932c27';
 
   @override
   Widget build(BuildContext context) {
+    checkPermission();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange,
@@ -27,13 +29,23 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: WebView(
-        onWebViewCreated: (WebViewController controller){
-          this.controller = controller;
-        },
-        initialUrl: homeUrl,
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          onWebViewCreated: (WebViewController controller) {
+            this.controller = controller;
+          },
+          initialUrl: homeUrl,
+          javascriptMode: JavascriptMode.unrestricted,
+          geolocationEnabled: true,
+        );
+      }),
     );
+  }
+
+  checkPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
   }
 }
