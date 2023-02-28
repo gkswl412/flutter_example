ffc6fd5cc4820e3fcaa882fc65113b624e96ab05
jeonghyeon2 <jeonghyeon@raycom.co.kr>
2023-02-28 18:20:20 +0900

    [2023-02-28] 홈 화면 구현 50%

--- actual/lib/common/layout/default_layout.dart
+++ actual/lib/common/layout/default_layout.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Widget child;
+  final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
   const DefaultLayout({
+    this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
     required this.child,
     Key? key,
   }) : super(key: key);
@@ -11,8 +17,30 @@ class DefaultLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if(title == null) {
+      return null;
+    }else {
+       return AppBar(
+         centerTitle: true,
+         backgroundColor: Colors.white,
+         foregroundColor: Colors.black,
+         elevation: 0,
+         title: Text(
+           title!,
+           style: TextStyle(
+             fontSize: 16.0,
+             fontWeight: FontWeight.w500,
+           ),
+         ),
+       );
+    }
   }
 }
