9509bb66625620973dca1c57cd04db4d5e615f12
jeonghyeon2 <gkswl412@naver.com>
2023-03-07 18:02:11 +0900

    [2023-03-07] 상태관리 프로젝트 적용 30%

---
+++ actual/lib/restaurant/provider/restaurant_provider.dart
@@ -0,0 +1,58 @@
+import 'package:actual/common/model/cursor_pagination_model.dart';
+import 'package:actual/common/model/pagination_params.dart';
+import 'package:actual/restaurant/model/restaurant_model.dart';
+import 'package:actual/restaurant/repository/restaurant_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final restaurantProvider =
+  StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>(
+    (ref) {
+      final repository = ref.watch(restaurantRepositoryProvider);
+
+      final notifier = RestaurantStateNotifier(repository: repository);
+
+      return notifier;
+    }
+  );
+
+class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
+  final RestaurantRepository repository ;
+
+  RestaurantStateNotifier({
+    required this.repository,
+  }): super(CursorPaginationLoading()){
+    paginate();
+  }
+
+  void paginate({
+    int fetchCount = 20,
+    // 추가로 데이터 더 가져오기
+    // true - 추가로 데이터 더 가져옴
+    // false - 새로고침 (현재 상태를 덮어씌움)
+    bool fetchMore = false,
+    // 강제로 다시 로딩하기
+    // true - CursorPaginationLoading()
+    bool forceRefetch = false,
+  }) async {
+    // 5가지 가능성
+    // state의 상태
+    // [상태가]
+    // 1) CursorPagination - 정상적으로 데이터가 있는 상태
+    // 2) CursorPaginationLoading - 데이터가 로딩중인 상태 (현재 캐시 없음)
+    // 3) CursorPaginationError - 에러가 있는 상태
+    // 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터를 가져올때
+    // 5) CursorPaginationFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을때
+
+    // 바로 반환하는 상황
+    // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고있다면)
+    // 2) 로딩중 - fetchMore : true
+    //    fetchMore가 아닐때 - 새로고침의 의도가 있다
+    if(state is CursorPagination && !forceRefetch) {
+      final pState = state as CursorPagination;
+
+      if(!pState.meta.hasMore) {
+        return;
+      }
+    }
+  }
+}
