804bd7e482e1cbd39a7436550c84d534a40e06a8
jeonghyeon2 <gkswl412@naver.com>
2023-03-13 13:31:39 +0900

    commit

---
+++ go_router_actual/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:go_router_actual/provider/auth_provider.dart';
+import 'package:go_router_actual/screen/1_screen.dart';
+import 'package:go_router_actual/screen/2_screen.dart';
+import 'package:go_router_actual/screen/3_screen.dart';
+import 'package:go_router_actual/screen/error_screen.dart';
+import 'package:go_router_actual/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: _App(),
+    ),
+  );
+}
+
+class _App extends ConsumerWidget {
+  _App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      // 라우터 정보를 전달
+      routeInformationProvider: router.routeInformationProvider,
+      // URI String을 상태 및 Go Router에서 사용할 수 있는 형태로 변환해주는 함수
+      routeInformationParser: router.routeInformationParser,
+      // 위에서 변경된 값으로 실제 어떤 라우트를 보여줄지 정하는 함수
+      routerDelegate: router.routerDelegate,
+    );
+  }
+}
